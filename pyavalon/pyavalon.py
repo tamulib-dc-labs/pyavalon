b78e5b4c831ab985429da6d903298f2f1d02b8df
Mark Baggett <markpbaggett@gmail.com>
2025-09-03 09:22:25 -0500

    Add get all file ids in a collection.

--- pyavalon/pyavalon.py
+++ pyavalon/pyavalon.py
@@ -1,6 +1,7 @@
 import click
 from pyavalon import AvalonMediaObject, AvalonCollection
 from pprint import pprint
+from csv import DictWriter
 
 @click.group()
 def cli() -> None:
@@ -18,3 +19,42 @@ def cli() -> None:
 def print_all_collections(instance):
     example = AvalonCollection("whatever", prod_or_pre=instance)
     pprint(example.get_all_collections())
+
+
+@cli.command(
+    "get_file_ids_from_a_collection", help="Find all the ids for master files in a collection"
+)
+@click.option(
+    "--collection",
+    "-c",
+    help="The id of the collection"
+)
+@click.option(
+    "--instance",
+    "-i",
+    help="The Avalon Instance you want",
+    default="pre"
+)
+@click.option(
+    "--output_csv",
+    "-o",
+    help="The Path to Where to Write Your CSV",
+    default="output.csv"
+)
+def get_file_ids_from_a_colleciton(collection, instance, output_csv):
+    final_files = []
+    current_collection = AvalonCollection(collection, prod_or_pre=instance)
+    all_items = current_collection.page_items()
+    for k, v in all_items.items():
+        all_files = v['files']
+        for file_id in all_files:
+            final_files.append(
+                {
+                    "id": file_id["id"],
+                    "label": file_id["label"]
+                }
+            )
+    with open(output_csv, 'w') as final_csv:
+        writer = DictWriter(final_csv, fieldnames=["id", "label"])
+        writer.writeheader()
+        writer.writerows(final_files)
